cce872fff45267b0864b82ecd7701a6e3496608b
OStap08 <ostapadamchuk@gmail.com>
2022-06-17 14:26:04 +0300

    added react-router-dom to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import Dashboard from "./pages/Dashboard";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 
 const App = () => {
   return (
-      <Fragment>
-          <Dashboard/>
-      </Fragment>
+      <BrowserRouter>
+          <Routes>
+              <Route path={'/dashboard'} element={<Dashboard/>} />
+              <Route
+                  path="*"
+                  element={<Navigate to="/dashboard" replace />}
+              />
+          </Routes>
+      </BrowserRouter>
   );
 };
 
