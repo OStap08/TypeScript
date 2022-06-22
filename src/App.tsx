93d158b7d5040e74da3311259639372e542834f2
OStap08 <ostapadamchuk@gmail.com>
2022-06-22 09:58:04 +0300

    refactored routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
-import Dashboard from "./pages/Dashboard";
-import { Routes, Route, Navigate} from "react-router-dom"
+import React, {FC} from 'react';
+import AppRouter from './router/AppRouter'
 
-const App = () => {
+const App: FC = () => {
     return (
-        <Routes>
-            <Route path={'/dashboard'} element={<Dashboard/>}/>
-            <Route
-                path="*"
-                element={<Navigate to="/dashboard" replace/>}
-            />
-        </Routes>
+        <>
+            <AppRouter/>
+        </>
     );
 };
 
